ddfb303557c4983aac7b87ce67e9dbaa9bb206e4
robinovitch61 <leorobinovitch@gmail.com>
2018-05-09 11:40:45 -0400

    add runall_wines script

---
+++ src/runall_wines.py
@@ -0,0 +1,12 @@
+import importlib
+
+# Import modules
+subset = importlib.import_module('.data.01_subset-data-GBP', 'src')
+plotwines = importlib.import_module('.visualization.02_visualize-wines', 'src')
+country_sub = importlib.import_module('.data.03_country-subset', 'src')
+
+fname = subset.process_data_GBP('data/raw/winemag-data-130k-v2.csv')
+print(fname)
+plotwines.create_plots(fname)
+country_fname = country_sub.get_country(fname, 'Chile')
+print(country_fname)
